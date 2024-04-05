c0ee1121323104e29fb3dbdf02b2be6b5931dda4
ElizabethLankford <elizabethlankford1@gmail.com>
2024-04-05 09:39:36 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Elizabeth Lankford</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
