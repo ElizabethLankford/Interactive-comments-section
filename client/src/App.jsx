fc73d759322d447d654341db51c13f2498028b1a
ElizabethLankford <elizabethlankford1@gmail.com>
2024-04-10 07:53:53 -0500

    connected client to server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Comments from "./components/Comments";
 import Footer from "./components/Footer";
+import { PostList } from "./components/PostList";
 import "./App.css";
 
 function App() {
   return (
     <div className="body">
+      <PostList />
       <Comments />
       <Footer />
     </div>
--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 import fastify from "fastify";
 import dotenv from "dotenv";
 import sensible from "@fastify/sensible";
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 const app = fastify();
 app.register(sensible);
+app.register(cors, {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+});
+
 const prisma = new PrismaClient();
 
 app.get("/posts", async (req, res) => {
+  // const posts = await prisma.post.findMany({
+  //   select: {
+  //     id: true,
+  //     title: true,
+  //   },
+  // });
+  // res.send(posts);
   return await commitToDb(
     prisma.post.findMany({
       select: {
