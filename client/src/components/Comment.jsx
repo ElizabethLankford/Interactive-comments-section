49a07ac23433cfa7da0a430f96037b74c711298f
ElizabethLankford <elizabethlankford1@gmail.com>
2024-04-12 11:16:46 -0500

    added icons for comments

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,3 +1,29 @@
+import IconBtn from "./IconBtn";
+import { FaEdit, FaHeart, FaReply, FaTrash } from "react-icons/fa";
+
+const dateFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "medium",
+  timeStyle: "short",
+});
+
 export default function Comment({ id, message, user, createdAt }) {
-  return <div key={id}>{message}</div>;
+  return (
+    <>
+      <div>
+        <div>
+          <span>{user.name}</span>
+          <span>{dateFormatter.format(Date.parse(createdAt))}</span>
+        </div>
+        <div>{message}</div>
+        <div>
+          <IconBtn Icon={FaHeart} aria-lable="Like">
+            2
+          </IconBtn>
+          <IconBtn Icon={FaReply} aria-lable="Reply" />
+          <IconBtn Icon={FaEdit} aria-lable="Edit" />
+          <IconBtn Icon={FaTrash} aria-lable="Delete" />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ client/src/components/IconBtn.jsx
@@ -0,0 +1,13 @@
+export default function IconBtn({ Icon, isActive, color, children, ...props }) {
+  return (
+    <button
+      className={`btn icon-btn ${isActive ? "active" : ""} ${color || ""}`}
+      {...props}
+    >
+      <span className={`${children != null ? "mr-1" : ""}`}>
+        <Icon />
+      </span>
+      {children}
+    </button>
+  );
+}
