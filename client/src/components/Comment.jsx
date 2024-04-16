393ff9dbc6583bc989ce37e538e327f465f9cf96
ElizabethLankford <elizabethlankford1@gmail.com>
2024-04-16 14:37:57 -0500

    set up reply logic

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,6 +4,9 @@ import { usePost } from "../contexts/PostContext";
 import { CommentList } from "./CommentList";
 import IconBtn from "./IconBtn";
 import { FaEdit, FaHeart, FaReply, FaTrash } from "react-icons/fa";
+import { CommentForm } from "./CommentForm";
+import { useAsyncFN } from "../hooks/useAsync";
+import { createComment } from "../services/comments";
 
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: "medium",
@@ -11,9 +14,21 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 export default function Comment({ id, message, user, createdAt }) {
-  const { getReplies } = usePost();
+  const { post, getReplies, createLocalComment } = usePost();
+  const createCommentFn = useAsyncFN(createComment);
   const childComments = getReplies(id);
   const [areChildrenHidden, setAreChildrenHidden] = useState(false);
+  const [isReplying, setIsReplying] = useState(false);
+
+  function onCommentReply(message) {
+    return createCommentFn
+      .execute({ postId: post.id, message, parentId: id })
+      .then((comment) => {
+        setIsReplying(false);
+        createLocalComment(comment);
+      });
+  }
+
   return (
     <>
       <div>
@@ -26,10 +41,25 @@ export default function Comment({ id, message, user, createdAt }) {
           <IconBtn Icon={FaHeart} aria-label="Like">
             2
           </IconBtn>
-          <IconBtn Icon={FaReply} aria-label="Reply" />
+          <IconBtn
+            onClick={() => setIsReplying((prev) => !prev)}
+            isActive={isReplying}
+            Icon={FaReply}
+            aria-label={isReplying ? "Cancel Reply" : "Reply"}
+          />
           <IconBtn Icon={FaEdit} aria-label="Edit" />
           <IconBtn Icon={FaTrash} aria-label="Delete" />
         </div>
+        {isReplying && (
+          <div>
+            <CommentForm
+              autoFocus
+              onSubmit={onCommentReply}
+              loading={createCommentFn.loading}
+              error={createCommentFn.error}
+            />
+          </div>
+        )}
         {childComments?.length > 0 && (
           <>
             <div
